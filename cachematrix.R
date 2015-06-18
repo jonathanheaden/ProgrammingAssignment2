2b3c7b1e4a97c8d45d8fe689dc27654792c834bd
Jonathan Headen <jonathan.headen@outlook.com>
2015-06-18 16:54:33 +1000

    functions working, added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,15 @@
 ## functions do
 
 ## Write a short comment describing this function
-## This function creates a special "matrix" object that can cache its inverse.
+## This function creates a closure to store 4 functions; get & set the matrix & cached inverse 
+## Inverse of x needs to be initialised (and reset in set function) to null to allow testing for existence of cached result
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- matrix() #inverse of x
-    set <- function(newInverse) {
-        m <<- newInverse
+    m <- NULL 
+    #internalMatrix <<- x
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        m <<- NULL
     }
     get <- function() x
     setinverse <- function(inverseOfM) m <<- inverseOfM
@@ -15,21 +18,25 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse )
+    
 }
 
 
 ## Write a short comment describing this function
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+## This function works on a special makeCacheMatrix object created with the above function. 
+## This function checks if there is a previuosly computed inverse of the matrix and returns that if present
+## otherwise it computes the inverse and stores that in cache
+## the object resets the cached inverse if the internal matrix is changed 
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     cachedInverse <- x$getinverse()
+    matrixToSolve <- x$get()
     if(!is.null(cachedInverse)) {
         cachedInverse$get()
     }
     else {
-        newInverse <- solve(x)
+        newInverse <- solve(matrixToSolve)
         x$setinverse(newInverse)
         newInverse
     }
